901d07dd462732cb864f1bf0a2f8b34c8358036c
Ellis Breen <ellis.breen@couchbase.com>
2018-11-12 12:37:08 +0000

    Optional Platform, Python Version and Python Arch parameters

--- python/python-scripted-build-pipeline.groovy
+++ python/python-scripted-build-pipeline.groovy
@@ -1,7 +1,10 @@
-def PLATFORMS = [ "centos7", "windows-2012" ]
+def parseCSVLine(String line){
+    return String(line).split(/\,\s*/)
+}
+def PLATFORMS =  parseCSVLine("${PLATFORMS}") || [ "centos7", "windows-2012" ]
 def DEFAULT_PLATFORM = PLATFORMS[0]
-def PY_VERSIONS = [ "2.7.15", "3.7.0" ] // 3.7.0 is failing tests
-def PY_ARCHES = [ "x64", "x86" ]
+def PY_VERSIONS = parseCSVLine("${PY_VERSIONS}") || [ "2.7.15", "3.7.0" ]
+def PY_ARCHES = parseCSVLine("${PY_ARCHES}") || [ "x64", "x86" ]
 def DEFAULT_PY_VERSION = PY_VERSIONS[0]
 def DEFAULT_VERSION_SHORT=DEFAULT_PY_VERSION.tokenize(".")[0] + "." + DEFAULT_PY_VERSION.tokenize(".")[1]
 def DEFAULT_PY_ARCH = PY_ARCHES[0]
