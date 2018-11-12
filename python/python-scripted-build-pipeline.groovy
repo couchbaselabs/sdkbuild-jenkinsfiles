1d4c66a4456900dfdf3a42bec80429bf277ceebc
Ellis Breen <ellis.breen@couchbase.com>
2018-11-12 12:45:17 +0000

    Parameterise Parallelism

--- python/python-scripted-build-pipeline.groovy
+++ python/python-scripted-build-pipeline.groovy
@@ -1,6 +1,15 @@
 def parseCSVLine(String line){
     return String(line).split(/\,\s*/)
 }
+
+def booleanOr(String line, Boolean fallBack = False)
+{
+    if (!line.size()){
+        return fallBack
+    }
+    return line.toBoolean()
+}
+
 def PLATFORMS =  parseCSVLine("${PLATFORMS}") || [ "centos7", "windows-2012" ]
 def DEFAULT_PLATFORM = PLATFORMS[0]
 def PY_VERSIONS = parseCSVLine("${PY_VERSIONS}") || [ "2.7.15", "3.7.0" ]
@@ -8,7 +17,7 @@ def PY_ARCHES = parseCSVLine("${PY_ARCHES}") || [ "x64", "x86" ]
 def DEFAULT_PY_VERSION = PY_VERSIONS[0]
 def DEFAULT_VERSION_SHORT=DEFAULT_PY_VERSION.tokenize(".")[0] + "." + DEFAULT_PY_VERSION.tokenize(".")[1]
 def DEFAULT_PY_ARCH = PY_ARCHES[0]
-
+def PARALLEL_PAIRS = booleanOr("${PARALLEL_PAIRS}",True)
 pipeline {
     agent none
     stages {
@@ -407,6 +416,8 @@ EOF
             }
         }
     }
-
-    parallel pairs
+    if (PARALLEL_PAIRS)
+    {
+        parallel pairs
+    }
 }
