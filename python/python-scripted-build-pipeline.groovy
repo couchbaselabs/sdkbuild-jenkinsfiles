abc28d570d07759843cafa4a673b38a916653683
Ellis Breen <ellis.breen@couchbase.com>
2018-11-15 21:33:59 +0000

    Upgrade to full admin

--- python/python-scripted-build-pipeline.groovy
+++ python/python-scripted-build-pipeline.groovy
@@ -515,7 +515,7 @@ void testAgainstServer(serverVersion, platform, envStr, testActor) {
             shWithEcho("cbdyncluster ${cmd_str} --bucket default setup " + clusterId)
             // Make the bucket flushable
             shWithEcho("curl -v -X POST -u Administrator:password -d flushEnabled=1 http://" + ip + ":8091/pools/default/buckets/default")
-            shWithEcho("""curl -X PUT --data "name=default&roles=full_admin&password=password" \
+            shWithEcho("""curl -X PUT --data "name=default&roles=admin&password=password" \
             -H "Content-Type: application/x-www-form-urlencoded" \
             http://Administrator:password@${ip}:8091/settings/rbac/users/local/default
             """)
