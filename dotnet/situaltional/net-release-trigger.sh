7853f67ac81744eac74eb3faee1a4e07c1c982aa
Mike Goldsmith <goldsmith.mike@gmail.com>
2019-05-28 15:01:02 +0100

    dotnet: update trigger variables to use NET_CLIENT_BRANCH

--- dotnet/situaltional/net-release-trigger.sh
+++ dotnet/situaltional/net-release-trigger.sh
@@ -12,26 +12,26 @@ echo CLIENT_COMMIT=$(git rev-parse --short HEAD)
 
 
 #watson (4.6.3) sync
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-watson-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-watson-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 #watson (4.6.3) async
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-watson-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-watson-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 
 #vulcan (5.5.0) sync
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-vulcan-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-vulcan-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 #vulcan (5.5.0) async
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-vulcan-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-vulcan-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 
 #alice (6.0.0) sync
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-alice-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-alice-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 #alice (6.0.0) async
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-alice-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/net-centos-alice-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 
 #netcore windows watson (4.5.1) sync
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/netcore-windows-watson-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/netcore-windows-watson-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 #netcore windows watson (4.5.1) async
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/netcore-windows-watson-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/netcore-windows-watson-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&NET_CLIENT_BRANCH=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 
 #netcore ubuntu watson (4.5.1)  sync
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/docker-netcore-ubuntu-watson-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/docker-netcore-ubuntu-watson-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
 #netcore ubuntu watson (4.5.1) ubuntu async
-wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/docker-netcore-ubuntu-watson-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_commit&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
+wget "http://sdkbuilds.sc.couchbase.com/view/.NET/job/sdk-net-situational-release/job/docker-netcore-ubuntu-watson-async-vs2017/buildWithParameters?token=sdkbuilds&useSSL=$ssl&client_version=$net_client_branch&GERRIT_COMMIT=$gerrit_commit&SDKDCLIENT_NG_BRANCH=$sdkd_commit"
