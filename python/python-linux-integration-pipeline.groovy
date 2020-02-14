07524bc420125c5d1ca906a4215b83ff8e4d1046
David Kelly <davidmichaelkelly@gmail.com>
2020-02-13 17:06:55 -0700

    Python: working (but still janky) integration pipeline

--- python/python-linux-integration-pipeline.groovy
+++ python/python-linux-integration-pipeline.groovy
@@ -17,118 +17,125 @@ class DynamicCluster {
     }
 }
 
-def CLUSTER = new DynamicCluster()
+def CLUSTER = [:]
 
-def collectStages() {
-    def pythons = "${PY_VERSIONS}".split()
-    def servers = "${SERVER_VERSIONS}".split()
-    def stages = [:]
-    for(py_version in pythons) {
-        for (server_vesion in servers) {
-            stages["${py_version}_${server_version}"] = doStages(py_version, server_version)
-        }
-    }
-    stages
-}
-
-def doStages(py_version, server_version) {
-    return stages {
-        stage("build_${py_version}_${server_version}") {
-            agent { label 'sdk-integration-test-linux' }
-            steps {
-                cleanWs()
-                dir("couchbase-python-client-${py_version}-${server_version}") {
-                    checkout([$class: 'GitSCM', branches: [[name: '$SHA']], userRemoteConfigs: [[refspec: "$GERRIT_REFSPEC", url: '$REPO', poll: false]]])
-                    shWithEcho("curl -o update_tests.py ${UPDATE_TESTS_URL}")
-                    shWithEcho("cat update_tests.py")
-                    shWithEcho("cbdep install python ${py_version} -d deps")
-                    withEnv(getEnvStr("${py_version}", "${server_version}")) {
-                        // source the venv activate script
-                        shWithEcho(". ./deps/python${py_version}/bin/activate")
-                        shWithEcho("python --version")
-                        shWithEcho("pip --version")
-                        shWithEcho("pip install -r dev_requirements.txt")
-                        shWithEcho("pip install cython")
-                        shWithEcho("pip install nose")
-                        // everything installed, lets build in place
-                        shWithEcho("python setup.py build_ext --inplace")
-                    }
-                }
-                stash includes: "couchbase-python-client-${py_version}-${server_version}/", name: 'python-client', useDefaultExcludes: false
-            }
-            stage('prepare cluster') {
-                agent { label 'sdk-integration-test-linux' }
-                steps {
-                    sh("cbdyncluster ps -a")
-                    script {
-                        CLUSTER.id = sh(script: "cbdyncluster allocate --num-nodes=3 --server-version=${server_vesion}", returnStdout: true).trim()
-                        CLUSTER.connstr = sh(script: "cbdyncluster ips ${CLUSTER.id}", returnStdout: true).trim()
-                        echo "Allocated ${CLUSTER.inspect()}"
-
-                        sh("cbdyncluster setup ${CLUSTER.id} --node=kv,index,n1ql,fts,cbas --node=kv --node=kv --bucket=default")
-                        // setup buckets, users
-                        shWithEcho("curl -v -X POST -u Administrator:password -d flushEnabled=1 http://${CLUSTER.getFirstIp()}:8091/pools/default/buckets/default")
-                        shWithEcho("""curl -v -X POST -u Administrator:password -d '["beer-sample"]' http://${CLUSTER.getFirstIp()}:8091/sampleBuckets/install""")
-                        sleep(30)
-                        shWithEcho("""curl -X PUT --data "name=default&roles=admin&password=password" \
-                            -H "Content-Type: application/x-www-form-urlencoded" \
-                            http://Administrator:password@${CLUSTER.getFirstIp()}:8091/settings/rbac/users/local/default
-                        """)
-                        shWithEcho("curl -X GET http://Administrator:password@${CLUSTER.getFirstIp()}:8091/settings/rbac/users")
-                        shWithEcho("curl http://Administrator:password@${CLUSTER.getFirstIp()}:8091/pools/default/buckets/default")
-                        shWithEcho("curl -vv -XPOST http://Administrator:password@${CLUSTER.getFirstIp()}:8091/settings/developerPreview -d 'enabled=true'")
-                    }
-                }
-            }
-            stage("test-${py_version}-${server_version}") {
-                post {
-                    always {
-                        junit "couchbase-python-client-${py_version}-${server_version}/nosetests.xml"
-                        script {
-                            if (CLUSTER.isAllocated()) {
-                                sh("cbdyncluster rm ${CLUSTER.id}")
-                                echo("removed cluster")
-                            }
-                        }
-                    }
-                }
-                agent { label 'sdk-integration-test-linux' }
-                steps {
-                    unstash "python-client-${py_version}-${server_version}"
-                    dir("couchbase-python-client-${py_version}-${server_version}"){
-                        withEnv(getEnvStr("${py_version}", "${server_version}")) {
-                            shWithEcho(". ./deps/python${py_version}/bin/activate")
-                            shWithEcho("python --version")
-                            shWithEcho("python update_tests.py ${CLUSTER.getFirstIp()}")
-                            shWithEcho("pip install -r requirements.txt")
-                            shWithEcho("${NOSE_COMMAND}")
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-// TODO: take a hard look at whether or not this is necessary
+// TODO: Add the globals above, etc...
 def getEnvStr(pyversion, server_version) {
-    return ["PATH=${WORKSPACE}/couchbase-python-client/deps/python${pyversion}-amd64:${WORKSPACE}/couchbase-python-client/deps/python${pyversion}-amd64/bin:${WORKSPACE}/couchbase-python-client/deps/python${pyversion}:${WORKSPACE}/couchbase-python-client/deps/python${pyversion}/bin:${WORKSPACE}/deps/valgrind/bin/:$PATH", "LCB_PATH=${WORKSPACE}/libcouchbase"]
- }
+    return ["PATH=${WORKSPACE}/couchbase-python-client-${pyversion}-${server_version}/deps/python${pyversion}-amd64:${WORKSPACE}/couchbase-python-client-${pyversion}-${server_version}/deps/python${pyversion}-amd64/bin:${WORKSPACE}/couchbase-python-client-${pyversion}-${server_version}/deps/python${pyversion}:${WORKSPACE}/couchbase-python-client-${pyversion}-${server_version}/deps/python${pyversion}/bin:${WORKSPACE}/deps/valgrind/bin/:$PATH", "LCB_PATH=${WORKSPACE}/libcouchbase"]
+}
 
 void shWithEcho(String command) {
     echo sh (script: command, returnStdout: true)
 }
 
+def doStages(UPDATE_TESTS_URL, CLUSTER, NOSE_COMMAND) {
+    def pythons = "${PY_VERSIONS}".split()
+    def servers = "${SERVER_VERSIONS}".split()
+    echo "${pythons} | ${servers}"
+    def mystages = [:]
+    for(p in pythons) {
+        def py_version = p
+        for (s in servers) {
+            def server_version = s
+            echo "creating stages ${py_version}-${server_version}"
+            mystages["${py_version}_${server_version}"] = {
+                node('sdk-integration-test-linux') {
+                    stage("build ${py_version}-${server_version}") {
+                        cleanWs()
+                        dir("couchbase-python-client-${py_version}-${server_version}") {
+                            checkout([$class: 'GitSCM', branches: [[name: '$SHA']], userRemoteConfigs: [[refspec: "$GERRIT_REFSPEC", url: '$REPO', poll: false]]])
+                            shWithEcho("curl -o update_tests.py ${UPDATE_TESTS_URL}")
+                            shWithEcho("cat update_tests.py")
+                            shWithEcho("cbdep install python ${py_version} -d deps")
+                            withEnv(getEnvStr("${py_version}", "${server_version}")) {
+                                shWithEcho("python --version")
+                                shWithEcho("pip --version")
+                                shWithEcho("pip install -r dev_requirements.txt")
+                                shWithEcho("pip install cython")
+                                shWithEcho("pip install nose")
+                                // everything installed, lets build in place
+                                shWithEcho("python setup.py build_ext --inplace")
+                            }
+                        }
+                        stash includes: "couchbase-python-client-${py_version}-${server_version}/", name: "python-client-${py_version}-${server_version}", useDefaultExcludes: false
+                    }
+                    stage("prepare-cluster-and-test ${py_version}-${server_version}") {
+                        sh("cbdyncluster ps -a")
+                        try {
+                            def cluster = new DynamicCluster()
+                            CLUSTER["${py_version}-${server_version}"] = cluster
+                            def alloc = sh(script: "cbdyncluster allocate --num-nodes=3 --server-version=${server_version}", returnStdout: true)
+                            echo "allocate returned: ${alloc}"
+                            cluster.id = alloc.trim()
+                            def ips = sh(script: "cbdyncluster ips ${cluster.id}", returnStdout: true)
+                            echo "ips returned: ${ips}"
+                            cluster.connstr = ips.trim()
+                            sh("cbdyncluster setup ${cluster.id} --node=kv,index,n1ql,fts,cbas --node=kv --node=kv --bucket=default")
+                            // setup buckets, users, storage mode
+                            shWithEcho("curl -v -X POST -u Administrator:password -d flushEnabled=1 http://${cluster.getFirstIp()}:8091/pools/default/buckets/default")
+                            shWithEcho("""curl -v -X POST -u Administrator:password -d '["beer-sample"]' http://${cluster.getFirstIp()}:8091/sampleBuckets/install""")
+                            shWithEcho("curl -vv -X POST -u Administrator:password -d'storageMode=plasma' http://${cluster.getFirstIp()}:8091/settings/indexes")
+                            sleep(30)
+                            shWithEcho("""curl -X PUT --data "name=default&roles=admin&password=password" \
+                                -H "Content-Type: application/x-www-form-urlencoded" \
+                                http://Administrator:password@${cluster.getFirstIp()}:8091/settings/rbac/users/local/default
+                            """)
+                            shWithEcho("curl -X GET http://Administrator:password@${cluster.getFirstIp()}:8091/settings/rbac/users")
+                            shWithEcho("curl http://Administrator:password@${cluster.getFirstIp()}:8091/pools/default/buckets/default")
+                            // put primary index on default bucket.  TODO: make tests do this perhaps?  Also remember to not do this
+                            // after setting developer preview mode!!
+                            shWithEcho("curl -vv -XPOST http://Administrator:password@${cluster.getFirstIp()}:8093/query/service -d 'statement=CREATE PRIMARY INDEX ON `default`'")
+                            // Now an FTS index as well... Again fix the tests but later
+                            shWithEcho("""curl -X PUT -vv -u Administrator:password -d '{"name":"beer-search", "type":"fulltext-index", "sourceType":"couchbase", "sourceName":"beer-sample"}' -H 'Content-Type: application/json' http://${cluster.getFirstIp()}:8094/api/index/beer-search""")
+                            shWithEcho("curl -vv -XPOST http://Administrator:password@${cluster.getFirstIp()}:8091/settings/developerPreview -d 'enabled=true'")
+                            // sleep a bit to be sure indexes are ok
+                            sleep(30)
+                            unstash "python-client-${py_version}-${server_version}"
+                            dir("couchbase-python-client-${py_version}-${server_version}"){
+                                withEnv(getEnvStr("${py_version}", "${server_version}")) {
+                                    def ip = CLUSTER["${py_version}-${server_version}"].getFirstIp()
+                                    echo "ip: ${ip}"
+                                    shWithEcho("python --version")
+                                    shWithEcho("python update_tests.py ${ip}")
+                                    shWithEcho("pip install -r requirements.txt")
+                                    shWithEcho("${NOSE_COMMAND}")
+                                    echo "tests done"
+                                }
+                            }
+                        } catch(all) {
+                            echo "${py_version}-${server_version} tests had failures"
+                        } finally {
+                            if (fileExists("couchbase-python-client-${py_version}-${server_version}/nosetests.xml") ) {
+                                junit "couchbase-python-client-${py_version}-${server_version}/nosetests.xml"
+                            }
+                            script {
+                                def c = CLUSTER["${py_version}-${server_version}"]
+                                if (c.isAllocated()) {
+                                    sh("cbdyncluster rm ${c.id}")
+                                    echo "removed cluster"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    echo "${mystages}"
+    return mystages
+}
 
 pipeline {
-    agent none
+    agent { label 'sdk-integration-test-linux' }
     options {
         timeout(time: 90, unit: 'MINUTES')
     }
     stages {
         stage('create_builds') {
-            script {
-                collectStages()
+            steps {
+                script {
+                    parallel doStages(UPDATE_TESTS_URL, CLUSTER, NOSE_COMMAND)
+                }
             }
         }
     }
