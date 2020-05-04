5b44b849f3e42ab5daa5a12739f59736e79972c1
Ellis Breen <ellis.breen@couchbase.com>
2020-05-04 12:28:14 +0100

    Add 2.7.16 for PYCBC_VERSION==2.x

--- python/python-scripted-build-pipeline.groovy
+++ python/python-scripted-build-pipeline.groovy
@@ -1,6 +1,9 @@
 def PLATFORMS =  "${PLATFORMS}".split(/\s+/) ?: ["centos7", "windows-2012" ]
 def DEFAULT_PLATFORM = PLATFORMS[0]
 def PY_VERSIONS = "${PY_VERSIONS}"?"${PY_VERSIONS}".split(/\s+/): [ "3.7.6", "3.8.1" ]
+if ("${PYCBC_VERSION}" =~ "^2\\..*"){
+    PY_VERSIONS += ["2.7.16"]
+}
 def PY_ARCHES = "${PY_ARCHES}".split(/\s+/) ?: [ "x64", "x86" ]
 def SERVER_VERSIONS = "${SERVER_VERSIONS}"?[ "5.5.0", "6.0.0"]: "${SERVER_VERSIONS}".split(/\s+/)
 def PACKAGE_PLATFORM = "${DEFAULT_PLATFORM}"
