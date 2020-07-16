402ea8639aa251ce1ae95ca0efec4d1326b1384d
Sergey Avseyev <sergey.avseyev@gmail.com>
2020-07-16 10:45:45 +0300

    ruby: pass environment to scripts

--- ruby/ruby-scripted-build-pipeline.groovy
+++ ruby/ruby-scripted-build-pipeline.groovy
@@ -1,54 +1,78 @@
 // vim:et ts=4 sts=4 sw=4
 
 pipeline {
-    agent { label "sdkqe-centos8" }
+    agent none
     stages {
-        stage("deps") {
-            steps {
-                timestamps {
-                    cleanWs()
-                    dir("ruby-sdk") {
-                        checkout([
-                                $class: "GitSCM",
-                                branches: [[name: "$SHA"]],
-                                extensions: [[
-                                    $class: "SubmoduleOption",
-                                    disableSubmodules: false,
-                                    parentCredentials: true,
-                                    recursiveSubmodules: true,
-                                ]],
-                                userRemoteConfigs: [[
-                                    refspec: "$GERRIT_REFSPEC",
-                                    url: "$REPO",
-                                    poll: false
-                                ]]])
-
-                        sh("./bin/jenkins/install-dependencies")
+        stage('check') {
+            matrix {
+                axes {
+                    axis {
+                        name 'CB_RUBY_VERSION'
+                        values '2.5', '2.6', '2.7'
+                    }
+                    axis {
+                        name 'PLATFORM'
+                        values 'sdkqe-centos8', 'macos'
                     }
                 }
-            }
-        }
-        stage("build") {
-            steps {
-                timestamps {
-                    dir("ruby-sdk") {
-                        sh("./bin/jenkins/build-extension")
+                agent { label "macos" }
+                stages {
+                    stage("src") {
+                        steps {
+                            timestamps {
+                                cleanWs()
+                                dir("ruby-sdk-${CB_RUBY_VERSION}") {
+                                    checkout([
+                                        $class: "GitSCM",
+                                        branches: [[name: "$SHA"]],
+                                        extensions: [[
+                                            $class: "SubmoduleOption",
+                                            disableSubmodules: false,
+                                            parentCredentials: true,
+                                            recursiveSubmodules: true,
+                                        ]],
+                                        userRemoteConfigs: [[
+                                            refspec: "$GERRIT_REFSPEC",
+                                            url: "$REPO",
+                                            poll: false
+                                    ]]])
+                                }
+                            }
+                        }
                     }
-                }
-            }
-        }
-        stage("test") {
-            options {
-                timeout(time: 20, unit: 'MINUTES')
-            }
-            post {
-                always {
-                    junit("ruby-sdk/test/reports/*.xml")
-                }
-            }
-            steps {
-                dir("ruby-sdk") {
-                    sh("./bin/jenkins/test-with-cbdyncluster")
+                    stage("deps") {
+                        steps {
+                            timestamps {
+                                dir("ruby-sdk-${CB_RUBY_VERSION}") {
+                                    sh("./bin/jenkins/install-dependencies")
+                                }
+                            }
+                        }
+                    }
+                    stage("build") {
+                        steps {
+                            timestamps {
+                                dir("ruby-sdk-${CB_RUBY_VERSION}") {
+                                    sh("./bin/jenkins/build-extension")
+                                }
+                            }
+                        }
+                    }
+                    stage("test") {
+                        options {
+                            timeout(time: 20, unit: 'MINUTES')
+                        }
+                        post {
+                            always {
+                                junit("ruby-sdk-${CB_RUBY_VERSION}/test/reports/*.xml")
+                            }
+                        }
+                        steps {
+                            dir("ruby-sdk-${CB_RUBY_VERSION}") {
+                                sh("./bin/jenkins/test-with-cbdyncluster")
+                            }
+                        }
+                    }
                 }
             }
         }
