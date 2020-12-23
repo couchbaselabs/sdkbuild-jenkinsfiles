747fa945d855f8288bad1ac28f6a110f7d79cc1f
Sergey Avseyev <sergey.avseyev@gmail.com>
2020-12-23 20:15:23 +0300

    ruby: update server versions

--- ruby/ruby-build-pipeline.groovy
+++ ruby/ruby-build-pipeline.groovy
@@ -138,8 +138,7 @@ pipeline {
                 axes {
                     axis {
                         name 'CB_VERSION'
-                        // values '6.0.4', '6.5.1', '6.6.0', '7.0.0-3739'
-                        values '6.0.4', '6.5.1', '7.0.0-3739'
+                        values '6.0.5', '6.5.2', '6.6.1', '7.0.0-4023'
                     }
                     axis {
                         name 'CB_RUBY_VERSION'
