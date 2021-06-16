92aa818d83a227c2ea38693dd0ae9add35814590
Sergey Avseyev <sergey.avseyev@gmail.com>
2021-06-16 16:19:58 +0300

    libcouchbase: ensure gdb is installed on the boxes

--- libcouchbase/lcb-lnx-scripted-build-pipeline.groovy
+++ libcouchbase/lcb-lnx-scripted-build-pipeline.groovy
@@ -217,6 +217,7 @@ pipeline {
                             }
                             steps {
                                 dir('ws_debian9_x64/build') {
+                                    sh("sudo apt update; sudo apt install -y gdb");
                                     sh("ulimit -c; cat /proc/sys/kernel/core_pattern || true")
                                     sh("ctest ${VERBOSE.toBoolean() ? '-VV' : ''}")
                                 }
@@ -273,6 +274,7 @@ pipeline {
                             }
                             steps {
                                 dir('ws_centos7_x64/build') {
+                                    sh("sudo yum install -y gdb");
                                     sh("ulimit -c; cat /proc/sys/kernel/core_pattern || true")
                                     sh("ctest ${VERBOSE.toBoolean() ? '-VV' : ''}")
                                 }
@@ -335,6 +337,7 @@ pipeline {
                                 sh("sed -i s:/home/couchbase/jenkins/workspace/lcb/lcb-scripted-build-pipeline/ws_centos7_x64/build:\$(realpath .):g tests/CTestTestfile.cmake")
                                 sleep(20)
                                 timeout(time: 60, unit: 'MINUTES') {
+                                    sh("sudo yum install -y gdb");
                                     sh("ulimit -c; cat /proc/sys/kernel/core_pattern || true")
                                     sh("ctest -E BUILD ${VERBOSE.toBoolean() ? '-VV' : ''}")
                                 }
