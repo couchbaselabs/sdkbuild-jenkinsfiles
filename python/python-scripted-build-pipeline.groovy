c4cd9425d5b2f9a3985309b12ca85b4e2cc82909
Jared Casey <jared.casey@couchbase.com>
2021-07-22 17:42:08 -0500

    move mkdir() -> built in dir()

--- python/python-scripted-build-pipeline.groovy
+++ python/python-scripted-build-pipeline.groovy
@@ -626,7 +626,7 @@ List getNoseArgs(SERVER_VERSION, String platform, pyversion = "", TestParams tes
     {
         nosetests_args+="--xunit-testsuite-name=${test_rel_path} --xunit-prefix-with-testsuite-name "
     }
-    mkdir(test_full_path, platform)
+    dir(test_full_path)
     [test_rel_path, nosetests_args, test_full_path, runner_command, post_command]
 }
 
