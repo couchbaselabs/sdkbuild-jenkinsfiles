a247983124fe3c96ad219e6a6f65b0f704222dc1
Sergey Avseyev <sergey.avseyev@gmail.com>
2023-05-08 11:10:24 +0300

    c++: template for sanitizer job definition

---
+++ cxx/cxx-sanitizers-pipeline.groovy
@@ -0,0 +1,122 @@
+// vim:et ts=4 sts=4 sw=4
+
+def check_clang() {
+    sh("""
+if [ ! -e /usr/bin/clang-16 ]
+then
+    curl https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+    sudo bash -c "echo 'deb https://apt.llvm.org/focal/ llvm-toolchain-focal-16 main' >> /etc/apt/sources.list"
+    sudo apt-get update -y
+    sudo apt-get install -y clang-16 clang-tools-16
+fi
+""")
+}
+
+def check_valgrind() {
+    sh("""
+if [ ! -e /usr/bin/valgrind ]
+then
+    sudo apt-get update -y
+    sudo apt-get install -y valgrind
+fi
+""")
+}
+
+def check_dependencies(tool) {
+    if (tool == "drd" || tool == "memcheck") {
+        check_valgrind()
+    } else {
+        check_clang()
+    }
+}
+
+pipeline {
+    agent none
+    stages {
+        stage('prep') {
+            agent any
+            steps {
+                script {
+                    buildName("sanitizers-${BUILD_NUMBER}")
+                }
+            }
+        }
+        stage("src") {
+            agent any
+            steps {
+                dir("couchbase-cxx-client") {
+                    deleteDir()
+                    checkout([
+                        $class: "GitSCM",
+                        branches: [[name: "$SHA"]],
+                        extensions: [[
+                            $class: "SubmoduleOption",
+                            disableSubmodules: false,
+                            parentCredentials: true,
+                            recursiveSubmodules: true,
+                        ]],
+                        userRemoteConfigs: [[
+                            refspec: "$REFSPEC",
+                            url: "$REPO",
+                            poll: false
+                        ]]
+                    ])
+                }
+                stash includes: "couchbase-cxx-client/", name: "couchbase-cxx-client", useDefaultExcludes: false
+            }
+        }
+        stage('check') {
+            matrix {
+                axes {
+                    axis {
+                        name 'TOOL'
+                        values 'drd', 'memcheck', 'tsan', 'ubsan', 'asan', 'lsan', 'msan'
+                    }
+                }
+                agent { label 'ubuntu20' }
+                stages {
+                    stage("src") {
+                        steps {
+                            cleanWs()
+                            dir("build-${TOOL}-${BUILD_NUMBER}") {
+                                deleteDir()
+                                unstash("couchbase-cxx-client")
+                            }
+                        }
+                    }
+                    stage("deps") {
+                        steps {
+                            check_dependencies(TOOL)
+                        }
+                    }
+                    stage("build") {
+                        steps {
+                            timestamps {
+                                dir("build-${TOOL}-${BUILD_NUMBER}/couchbase-cxx-client") {
+                                    sh("echo build ${TOOL}")
+                                    sh("ls")
+                                }
+                            }
+                        }
+                    }
+                    stage("run") {
+                        options {
+                            timeout(time: 60, unit: 'MINUTES')
+                        }
+                        steps {
+                            timestamps {
+                                script { // use scripted pipeline for dynamic stage name
+                                    stage(TOOL) {
+                                        dir("build-${TOOL}/couchbase-cxx-client") {
+                                            sh("echo running ${TOOL}")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
