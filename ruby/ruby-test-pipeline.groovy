3c1f3a2a63c7f7ed4dff1cb967db51730593e0e3
Sergey Avseyev <sergey.avseyev@gmail.com>
2024-02-07 15:57:56 -0800

    ruby: add 7.6 release

--- ruby/ruby-test-pipeline.groovy
+++ ruby/ruby-test-pipeline.groovy
@@ -30,7 +30,7 @@ pipeline {
                 axes {
                     axis {
                         name 'CB_VERSION'
-                        values '7.2-stable', '7.1-release', '7.0-release', '6.6-release'
+                        values '7.6-stable', '7.2-release', '7.1-release', '7.0-release', '6.6-release'
                     }
                     axis {
                         name 'CB_RUBY_VERSION'
