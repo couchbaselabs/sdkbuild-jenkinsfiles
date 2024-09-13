435aa1f49b81266fdcbbef9cad6ff5260c0fccfe
Sergey Avseyev <sergey.avseyev@gmail.com>
2024-09-13 14:21:31 -0700

    libcouchbase: re-enable aarch64 build

--- libcouchbase/lcb-scripted-build-pipeline.groovy
+++ libcouchbase/lcb-scripted-build-pipeline.groovy
@@ -276,7 +276,7 @@ pipeline {
                 axes {
                     axis {
                         name 'PLATFORM'
-                        values "ubuntu20", "debian9", "centos7", "rockylinux9", "m1", "alpine", "qe-ubuntu20-arm64", "qe-ubuntu22-arm64", "qe-rhel9-arm64"
+                        values "ubuntu20", "debian9", "centos7", "rockylinux9", "m1", "qe-grav2-amzn2", "alpine", "qe-ubuntu20-arm64", "qe-ubuntu22-arm64", "qe-rhel9-arm64"
                     }
                 }
 
