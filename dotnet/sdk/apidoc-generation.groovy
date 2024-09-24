3273f57c3eee53545869e8aaff00e7228aa248f3
RiPont <richard.ponton@couchbase.com>
2024-09-24 15:20:46 -0700

    * separate into distinct steps * use updated couchbase-api-docfx project to build docs without powershell

--- dotnet/sdk/apidoc-generation.groovy
+++ dotnet/sdk/apidoc-generation.groovy
@@ -1,7 +1,8 @@
 // Please do not Save the Jenkins pipeline with a modified script.
 // Instead, use the Replay function to test build script changes, then commit final changes
 // to the sdkbuilds-jenkinsfile repository.
-def DOTNET_SDK_VERSION = "6.0.101"
+def DOTNET_SDK_VERSION = "8.0.401"
+def DOTNET_SDK_VERSIONS = ["6.0.425", DOTNET_SDK_VERSION]
 
 // Windows has a 255 character path limit, still.
 // cbdep trying to unzip .NET SDK 5.0.x shows a FileNotFound error, which is really a PathTooLong error
@@ -44,31 +45,59 @@ pipeline {
         }
         stage("build") {
             agent { label "windows" }
-            steps {
-                cleanWs(patterns: [[pattern: 'deps/**', type: 'EXCLUDE']])
-                unstash "${API_REPO_NAME}"
-                unstash "couchbase-apidocs-docfx"
-                installSDK("windows", DOTNET_SDK_VERSION)
-                script {
-                    env.PATH = "${PATH};${CBDEP_WIN_PATH}\\dotnet-core-sdk-${DOTNET_SDK_VERSION}"
-                    dir("couchbase-apidocs-docfx") {
-                        batWithEcho("powershell .\\Generate-ApiDocs.ps1 .\\${API_REPO_NAME}\\ -ApiVersion ${VERSION}") // -ZipOutput ${API_REPO_NAME}-${VERSION}.zip")
-                        dir("${API_REPO_NAME}") {
-                            zip dir: "_site", zipFile: "${API_REPO_NAME}-${VERSION}.zip", archive: true
-                            archiveArtifacts artifacts: "${API_REPO_NAME}-${VERSION}.zip", fingerprint: true
-                            stash includes: "${API_REPO_NAME}-${VERSION}.zip", name: "apidocs", useDefaultExcludes: false
-                            // // if (PUBLISH_TO_S3.toBoolean == true) {
-                            // //     dir("_site") {
-                            // //         withAWS(credentials: 'aws-sdk', region: 'us-west-1') {
-                            // //             s3Upload(
-                            // //                 bucket: 'docs.couchbase.com',
-                            // //                 path: "sdk-api/${API_REPO_NAME}-${VERSION}/",
-                            // //                 acl: 'PublicRead',
-                            // //                 file: 'docs/',
-                            // //             )
-                            // //         }       
-                            // //     }
-                            // // }
+            stages {
+                stage ("unstash") {
+                    steps {
+                        cleanWs(patterns: [[pattern: 'deps/**', type: 'EXCLUDE']])
+                        unstash "${API_REPO_NAME}"
+                        unstash "couchbase-apidocs-docfx"
+                    }
+                }
+                stage ("install-dotnet-sdks") {
+                    steps {
+                        installSdksForPlatform("windows", DOTNET_SDK_VERSIONS)
+                    }
+                }
+                stage ("build-sdk") {
+                    steps {
+                        script {
+                            def dotNetCmd = getDotnetCmd("windows")
+                            def dotNetDir = dotNetCmd[0..-7]
+                            withEnv(["PATH=${dotNetDir};${PATH}${dotNetDir}"]) {
+                                echo env.PATH
+                                batWithEcho("dotnet --list-sdks")
+                                batWithEcho("dotnet sdk check")
+                                dir("couchbase-net-client")
+                                {
+                                    batWithEcho("dotnet build src\\Couchbase\\Couchbase.csproj -c Release /p:Version=${VERSION}")
+                                }
+                            }
+                        }
+                    }
+                }
+                stage ("build-docs") {
+                    steps {
+                        script {
+                            def dotNetCmd = getDotnetCmd("windows")
+                            def dotNetDir = dotNetCmd[0..-7]
+                            withEnv(["PATH=${dotNetDir};${PATH}${dotNetDir}"]) {
+                                def year = new Date().year
+                                def copyright = "&copy; ${year + 1900} Couchbase, Inc."
+                                def overrides = ["_appTitle": VERSION, "_appFooter": copyright ]
+                                dir("couchbase-apidocs-docfx") {
+                                    writeJSON file:"overrides.json", json: overrides
+                                    batWithEcho("dotnet run --project DocFxRun\\DocFxRun.csproj ${API_REPO_NAME}\\docfx.json")
+                                    dir("${API_REPO_NAME}") {
+                                        def htmlFiles = findFiles(glob: "_site/api/*.html")
+                                        if (htmlFiles.length < 2) {
+                                            error "The site doesn't appear to have actually been generated, as there were no API HTML files in _site/api"
+                                        }
+                                        zip dir: "_site", zipFile: "${API_REPO_NAME}-${VERSION}.zip", archive: true
+                                        archiveArtifacts artifacts: "${API_REPO_NAME}-${VERSION}.zip", fingerprint: true
+                                        stash includes: "${API_REPO_NAME}-${VERSION}.zip", name: "apidocs", useDefaultExcludes: false
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -120,12 +149,37 @@ void batWithEcho(String command) {
     echo "[$STAGE_NAME]"+ bat (script: command, returnStdout: true)
 }
 
+def getDepsDir(PLATFORM) {
+    if (PLATFORM.contains("window")) {
+        return "${env.TEMP}\\cbnc\\deps"
+    }
+
+    return "deps"
+}
+
+def getDotnetCmd(PLATFORM) {
+    depsDir = getDepsDir(PLATFORM)
+    if (PLATFORM.contains("window")) {
+        return "${depsDir}\\dotnet-core-sdk-all\\dotnet"
+    }
+
+    return "${depsDir}//dotnet-core-sdk-all/dotnet"
+}
+
+// 'dotnet' understands forward-slashes on Windows, so the only difference is bat vs. sh
+def dotNetWithEcho(PLATFORM, command) {
+    dotNetCmd = getDotnetCmd(PLATFORM)
+    if (PLATFORM.contains("window")) {
+        batWithEcho("${dotNetCmd} ${command}")
+    } else {
+        shWithEcho("${dotNetCmd} ${command}")
+    }
+}
+
 def installSDK(PLATFORM, DOTNET_SDK_VERSION) {
     def install = false
-    def depsDir = "deps"
-    if (PLATFORM.contains("window")) {
-        depsDir = "%TEMP%\\cbnc\\deps"
-    }
+    def depsDir = getDepsDir(PLATFORM)
+
     dir(depsDir) {
         dir("dotnet-core-sdk-${DOTNET_SDK_VERSION}") {
             if (PLATFORM.contains("window")) {
@@ -139,12 +193,47 @@ def installSDK(PLATFORM, DOTNET_SDK_VERSION) {
     if (install) {
         echo "Installing .NET SDK ${DOTNET_SDK_VERSION}"
         if (PLATFORM.contains("window")) {
-            batWithEcho("cbdep install -d %TEMP%\\cbnc\\deps dotnet-core-sdk ${DOTNET_SDK_VERSION}")
+            batWithEcho("cbdep install -d ${depsDir} dotnet-core-sdk ${DOTNET_SDK_VERSION}")
         } else {
-            shWithEcho("cbdep install -d deps dotnet-core-sdk ${DOTNET_SDK_VERSION}")
+            shWithEcho("cbdep -V")
+            shWithEcho("cbdep --debug install -d deps dotnet-core-sdk ${DOTNET_SDK_VERSION}")
+            if(PLATFORM.contains("amzn2")) {
+            	//Required install for amazon linux 2 - related issue https://github.com/dotnet/runtime/issues/57983
+            	shWithEcho("sudo yum install -y libicu60")
+            }
         }
     }
     else {
         echo ".NET SDK ${DOTNET_SDK_VERSION} for ${PLATFORM} is already installed."
+    }
+
+    return depsDir
+}
+
+def installSdksForPlatform(PLATFORM, DOTNET_SDK_VERSIONS) {
+    def depsDir = getDepsDir(PLATFORM)
+    for (dnv in DOTNET_SDK_VERSIONS) {
+        installSDK(PLATFORM, dnv)
+    }
+
+    echo "Combining installed dotnet SDKs into dotnet-core-sdk-all"
+    // NOTE:  do these in order, even if the deps were already there, so we don't end up with SDK.older overwriting files form SDK.newer.
+    for (dnv in DOTNET_SDK_VERSIONS) {
+        dir(depsDir) {
+            dir ("dotnet-core-sdk-all") {
+                if (PLATFORM.contains("window")) {
+                    // Zip + Unzip is faster than copy, and windows doesn't care about executable bits
+                    // Xcopy might be faster, if we could get the parameters correct
+                    zipFile = "..\\dotnet-core-sdk-${dnv}-windows.zip"
+                    if (!fileExists(zipFile)) {
+                        zip dir: "..\\dotnet-core-sdk-${dnv}", zipFile: zipFile
+                    }
+                    unzip zipFile: zipFile, dir: "."
+                } else {
+                    // For UNIX, we use cp to preserve file permissions
+                    shWithEcho("cp -r ../dotnet-core-sdk-${dnv}/* .")
+                }
+            }
+        }
     }
 }
